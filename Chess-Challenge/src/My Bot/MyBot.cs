c8e574f6b48989ac96f6c0d8176207b29705cb73
dorinon <89009999+dorinon@users.noreply.github.com>
2023-07-21 18:14:00 +0300

    added simple eval position by piece point values

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -5,6 +5,16 @@ public class MyBot : IChessBot
     public Move Think(Board board, Timer timer)
     {
         Move[] moves = board.GetLegalMoves();
+        evalPos(board);
         return moves[0];
+    }
+
+    float evalPos(Board board){
+        int matrialValue = 0;
+        int[] pointValues = {100, 350, 350, 525, 1000, 10000};
+        for(int i = 0;i < 5; i++){
+            matrialValue += (board.GetAllPieceLists()[i].Count - board.GetAllPieceLists()[i + 6].Count) * pointValues[i];
+        }
+        return matrialValue;
     }
 }
