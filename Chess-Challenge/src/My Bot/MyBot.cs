fde9c51009bdb7e009a5c6db8bf6726db0153d38
dorinon <89009999+dorinon@users.noreply.github.com>
2023-07-21 19:08:02 +0300

    started working on search

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -8,24 +8,27 @@ public class MyBot : IChessBot
 
         Move moveToPlay = moves[0];
         int highestValueMove = 0;
-        foreach(Move move in moves){
-            board.MakeMove(move);
-            if(evalPos(board) > highestValueMove){
-                highestValueMove = evalPos(board);
-                moveToPlay = move;
-            }
-            board.UndoMove(move);
-        }
+        
         return moveToPlay;
         
     }
 
-    int evalPos(Board board){
+    Move evalPos(Board board){
+        int depth = 3;
         int matrialValue = 0;
         int[] pointValues = {100, 350, 350, 525, 1000, 10000};
-        for(int i = 0;i < 5; i++){
-            matrialValue += (board.GetAllPieceLists()[i].Count - board.GetAllPieceLists()[i + 6].Count) * pointValues[i];
+
+        Move[] legalMoves = board.GetLegalMoves();
+        Move bestMove = legalMoves[0];
+        int bestMoveValue = 0;
+        foreach(Move move in legalMoves){
+            for(int y = 0; y < depth; y++){
+                matrialValue = 0;
+                for(int i = 0;i < 5; i++){
+                    matrialValue += (board.GetAllPieceLists()[i].Count - board.GetAllPieceLists()[i + 6].Count) * pointValues[i];
+                }
+            }
         }
-        return matrialValue;
+        return bestMove;
     }
 }
