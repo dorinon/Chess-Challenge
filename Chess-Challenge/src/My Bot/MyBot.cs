23fa0765b7550dd26979eced5308f9e4a9d832af
dorinon <89009999+dorinon@users.noreply.github.com>
2023-07-23 12:30:13 +0300

    added minimax search with alpha beta pruning the pruning has a bug I cant figure out!!!

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,34 +1,60 @@
-﻿using ChessChallenge.API;
+﻿using System;
+using ChessChallenge.API;
 
 public class MyBot : IChessBot
 {
+    private int searchDepth = 3;
+    private Move bestMove;
+    int[] pointValues = {100, 350, 350, 525, 1000, 99999};
     public Move Think(Board board, Timer timer)
     {
-        Move[] moves = board.GetLegalMoves();
-
-        Move moveToPlay = moves[0];
-        int highestValueMove = 0;
-        
-        return moveToPlay;
-        
+        Minimax(board, searchDepth, int.MinValue, int.MaxValue);
+        return bestMove;
     }
 
-    Move evalPos(Board board){
-        int depth = 3;
-        int matrialValue = 0;
-        int[] pointValues = {100, 350, 350, 525, 1000, 10000};
+    private int Minimax(Board board, int depth, int alpha, int beta)
+    {
+        if (depth == 0)
+        {
+            int materialValue = 0;
+            PieceList[] pieceLists = board.GetAllPieceLists();
+            
+            for(int i = 0;i < 5; i++){
+                materialValue += (pieceLists[i].Count - pieceLists[i + 6].Count) * pointValues[i];
+            }
+            return materialValue;
+        }
 
-        Move[] legalMoves = board.GetLegalMoves();
-        Move bestMove = legalMoves[0];
-        int bestMoveValue = 0;
-        foreach(Move move in legalMoves){
-            for(int y = 0; y < depth; y++){
-                matrialValue = 0;
-                for(int i = 0;i < 5; i++){
-                    matrialValue += (board.GetAllPieceLists()[i].Count - board.GetAllPieceLists()[i + 6].Count) * pointValues[i];
-                }
+        int bestEval = board.IsWhiteToMove ? int.MinValue : int.MaxValue;
+        int moveEval;
+        foreach (Move move in board.GetLegalMoves())
+        {
+            board.MakeMove(move);
+            moveEval = Minimax(board, depth - 1, alpha, beta);
+            board.UndoMove(move);
+
+            if (board.IsWhiteToMove)
+            {
+                bestEval = Math.Max(bestEval, moveEval);
+                alpha = Math.Max(alpha, moveEval);
+            }
+            else
+            {
+                bestEval = Math.Min(bestEval, moveEval);
+                beta = Math.Min(beta, moveEval);
+            }
+            
+            if (beta <= alpha)
+            {
+                break;
+            }
+
+            if (depth == searchDepth && moveEval == bestEval)
+            {
+                bestMove = move;
             }
         }
-        return bestMove;
+        
+        return bestEval;
     }
 }
