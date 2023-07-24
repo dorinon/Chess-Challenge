3c979cf2f608417a493d2ce07a6017fe8ffa2c6a
dorinon <89009999+dorinon@users.noreply.github.com>
2023-07-24 14:51:44 +0300

    code optimized pruning not working

--- Chess-Challenge/src/Evil Bot/EvilBot.cs
+++ Chess-Challenge/src/Evil Bot/EvilBot.cs
@@ -13,7 +13,7 @@ namespace ChessChallenge.Example
         public Move Think(Board board, Timer timer)
         {
             Move[] allMoves = board.GetLegalMoves();
-
+            
             // Pick a random move to play if nothing better is found
             Random rng = new();
             Move moveToPlay = allMoves[rng.Next(allMoves.Length)];
--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -3,81 +3,74 @@ using ChessChallenge.API;
 
 public class MyBot : IChessBot
 {
-    private int searchDepth = 3;
+    private int maxDepth = 3;
     private Move bestMove;
     private Board board;
+    private int bigNum = 1000000;
     // Point values for each piece type for evaluation
     int[] pointValues = {100, 350, 350, 525, 1000, 99999};
     public Move Think(Board board, Timer timer)
     {
         this.board = board;
-        // Call the Minimax algorithm to find the best move
-        Minimax(searchDepth, int.MinValue, int.MaxValue);
         
+        // Call the Minimax algorithm to find the best move
+        Console.WriteLine(Minimax(maxDepth, -bigNum, bigNum, board.IsWhiteToMove ? 1 : -1) + "  " + bestMove);
         return bestMove;
     }
 
     // Minimax algorithm with alpha-beta pruning
-    private int Minimax(int depth, int alpha, int beta)
+    private int Minimax(int depth, int alpha, int beta, int color)
     {
         // If the search reaches the desired depth or the end of the game, evaluate the position and return its value
-        if (depth == 0)
+        if (depth == 0 || board.IsDraw() || board.IsInCheckmate())
         {
-            return EvaluateBoard();
+            if (board.IsDraw())
+            {
+                return 0;
+            }
+            if (board.IsInCheckmate())
+            {
+                return (bigNum - (maxDepth - depth)) * -color;
+            }
+            return EvaluateBoard(color);
         }
+        int bestEval = -bigNum * color;
+        int moveEval = bestEval;
 
-        int bestEval = board.IsWhiteToMove ? int.MinValue : int.MaxValue;
-        int moveEval;
-        
         // Generate and loop through all legal moves for the current player
         foreach (Move move in board.GetLegalMoves())
         {
             // Make the move on a temporary board and call Minimax recursively
             board.MakeMove(move);
-            moveEval = Minimax(depth - 1, alpha, beta);
-            board.UndoMove(move);
 
+            moveEval = Minimax(depth - 1, -beta, -alpha, -color);
+            
+            board.UndoMove(move);
+            /*alpha = Math.Max(alpha, moveEval);
+            if (alpha <= beta)
+            {
+                break;
+            }*/
             // Update the best evaluation and alpha/beta values based on whether it's a minimizing or maximizing player
-            if (board.IsWhiteToMove)
+            if (moveEval * color > bestEval * color)
             {
-                bestEval = Math.Max(bestEval, moveEval);
-                alpha = Math.Min(alpha, bestEval);
-            }
-            else
-            {
-                bestEval = Math.Min(bestEval, moveEval);
-                beta = Math.Max(beta, bestEval);
-            }
-            if (beta <= alpha)
-            {
-                return bestEval;
-            }
-            if (depth == searchDepth && moveEval == bestEval)
-            {
-                bestMove = move;
+                bestEval = moveEval;
+                if (depth == 3) bestMove = move;
             }
         }
         
         return bestEval;
     }
 
-    private int EvaluateBoard()
+    private int EvaluateBoard(int color)
     {
         int materialValue = 0;
+        int mobilityValue = board.GetLegalMoves().Length * color;
         PieceList[] pieceLists = board.GetAllPieceLists();
-
-        if (board.IsDraw())
-        {
-            return 0;
-        }
-        if (board.IsInCheckmate())
-        {
-            return board.IsWhiteToMove ? int.MaxValue : int.MinValue;
-        }
         // Loop through each piece type and add the difference in material value to the total
         for(int i = 0;i < 5; i++){
             materialValue += (pieceLists[i].Count - pieceLists[i + 6].Count) * pointValues[i];
         }
-        return materialValue;
+        return materialValue + mobilityValue;
     }
 }
