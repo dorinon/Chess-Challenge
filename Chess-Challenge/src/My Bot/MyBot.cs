561702a0b749379f6b6124a124fdd35a0c84843c
dorinon <89009999+dorinon@users.noreply.github.com>
2023-07-25 17:59:33 +0300

    switched foreach by for and increased depth to 4

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -13,7 +13,7 @@ public class MyBot : IChessBot
         this.board = board;
 
         // Call the Minimax algorithm to find the best move
-        Console.WriteLine(Search(maxDepth, int.MinValue, int.MaxValue, board.IsWhiteToMove ? 1 : -1) + "  " + bestMove + " is white turn: " + board.IsWhiteToMove);
+        Console.WriteLine(Search(maxDepth, -30000, 30000, board.IsWhiteToMove ? 1 : -1) + "  " + bestMove + " is white turn: " + board.IsWhiteToMove);
         return bestMove;
     }
 
@@ -23,27 +23,22 @@ public class MyBot : IChessBot
         // If the search reaches the desired depth or the end of the game, evaluate the position and return its value
         if (depth == 0 || board.IsDraw() || board.IsInCheckmate())
         {
-            if (board.IsDraw())
-            {
-                return 0;
-            }
-            if (board.IsInCheckmate())
-            {
-                return -(int.MaxValue - (maxDepth - depth));
-            }
+            if (board.IsDraw()) return 0;
+            
+            if (board.IsInCheckmate()) return -30000 + (maxDepth - depth);
+            
             return EvaluateBoard(color);
         }
-        int bestEval = int.MinValue;
-        int eval = bestEval;
-
+        Move[] legalMoves = board.GetLegalMoves();
+        int bestEval = -30000;
+        int eval;
         // Generate and loop through all legal moves for the current player
-        foreach (Move move in board.GetLegalMoves())
+        for (int i = 0; legalMoves.Length > i; i++)
         {
+            Move move = legalMoves[i];
             // Make the move on a temporary board and call search recursively
             board.MakeMove(move);
-
             eval = -Search(depth - 1, -beta, -alpha, -color);
-            
             board.UndoMove(move);
 
             // Update the best move and prune if necessary
@@ -54,10 +49,8 @@ public class MyBot : IChessBot
                 // Improve alpha
                 alpha = Math.Max(alpha, eval);
                 
-                if (alpha >= beta)
-                {
-                    break;
-                }
+                if (alpha >= beta) break;
+                
             }
         }
         
